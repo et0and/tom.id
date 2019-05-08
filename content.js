d6675b652c81a103684f3c7f509ad17be47f8676
Tom Yufu <42124348+et0and@users.noreply.github.com>
2019-05-08 22:55:56 +1200

    Add files via upload

---
+++ content.js
@@ -0,0 +1,239 @@
+var data = [
+  { "label": "1.txt",
+    "contents": "images that stay the same, only the cause has changed."
+  },
+  { "label": "2.txt",
+    "contents": "disappearing letter on a hot day."
+  },
+  { "label": "3.txt",
+    "contents": "the last time, until next time."
+  },
+  { "label": "4.txt",
+    "contents": "dive into the sea<br>to hear what it has to say<br>once you've heard enough<br>call it a day."
+  },
+  { "label": "work.txt",
+    "contents": "telephone someone<br>hold the phone to your heart<br>listen to each other’s heartbeat<br>when they fall in sync, hang up"
+  },
+  { "label": "5.txt",
+    "contents": "a library of sounds<br>the doors<br>the taps<br>the windows<br>the lights<br>the curtains<br>the appliances"
+  },
+  { "label": "room.txt",
+    "contents": "open the doors<br>open the windows<br>let the outside in<br>and the inside out"
+  },
+  { "label": "Concept",
+    "contents": "Real-time crowd sourced design?"
+  },
+  { "label": "Concept",
+    "contents": "Anticipatory design?"
+  },
+  { "label": "Idea",
+    "listName": "Possible Topics",
+    "list": ["privacy", "aesthetics", "reactions", "curation of identity", "disposable world", "crowdsourcing", "learning as play"]
+  },
+  { "label": "Idea",
+    "listName": "For Branding:",
+    "list": ["fun", "eclectic type selection", "eye-catching", "bright colors", "articulate system", "playful", "bold", "light-hearted"]
+  },
+  { "label": "Idea",
+    "listName": "Of future importance:",
+    "list": ["personal identity", "play/discovery", "experience as communication", "design will no longer be human"]
+  },
+  { "label": "pikachu.webp",
+    "imgpath": "pikachu.webp",
+    "link": "https://developer.mozilla.org/"
+  },
+  { "label": "moon.webp",
+    "imgpath": "moon.webp",
+    "link": "https://developer.mozilla.org/"
+  },
+  { "label": "Snippet",
+    "author": "Kevin Kelly",
+    "title": "What Technology Wants",
+    "link": "https://en.wikipedia.org/wiki/What_Technology_Wants",
+    "page": "197",
+    "quote": 'That transference is not inevitable, but it does happen. As machines take over more of what humans once did, we tend to do less of the familiar. We don\'t walk as much, letting our autos do our walking.... We don\'t read if we don\'t have to. We don\'t calculate. We are in the process of offloading our remembering to Google, and we are eager to stop cleaning as soon as those cleaning bots get cheap enough.'
+  },
+  { "label": "sky.test",
+    "imgpath": "stars.gif",
+    "link": "https://developer.mozilla.org/"
+  },
+  { "label": "lain.experiment",
+    "imgpath": "lain.gif",
+    "link": "https://developer.mozilla.org/"
+  },
+  { "label": "hi.gif",
+    "imgpath": "sg.gif",
+    "link": "https://developer.mozilla.org/"
+  },
+  { "label": "lain.webp",
+    "imgpath": "lain.webp",
+    "link": "https://developer.mozilla.org/"
+  },
+  { "label": "lain.kb",
+    "imgpath": "keyboard.gif",
+    "link": "https://developer.mozilla.org/"
+  },
+  { "label": "lain.two",
+    "imgpath": "lain2.gif",
+    "link": "https://developer.mozilla.org/"
+  },
+  { "label": "Snippet",
+    "author": "Kevin Kelly",
+    "title": "What Technology Wants",
+    "link": "https://en.wikipedia.org/wiki/What_Technology_Wants",
+    "page": "233",
+    "quote": 'So why not steer everyone in this direction? Why don\'t we all give up more choices and become Amish? After all, Wendell Berry and the Amish see our multimillion choices as illusory and meaningless, or as choices that are really entrapments. I believe these two different routes for technological lifestyle—either optimizing contentment or optimizing choices—come down to very different ideas of what humans are to be.'
+  },
+  { "label": "Snippet",
+    "author": "Jessica Helfand",
+    "title": "Design: The Invention of Desire",
+    "link": "https://inventionofdesire.com/",
+    "page": "126",
+    "quote": 'If we\'re not favoriting and forwarding anonymously, we\'re claiming some modicum of ownership, merely by association—which at its core is stunningly inauthentic'
+  },
+  { "label": "Snippet",
+    "author": "Jessica Helfand",
+    "title": "Design: The Invention of Desire",
+    "link": "https://inventionofdesire.com/",
+    "page": "126",
+    "quote": 'And just what is it we\'re sharing? Fleeting glimpses, memes and tropes, regurgitated content produced by others? In many cases, we don\'t share; we reshare, positioning ourselves not so much as makers but as carriers...'
+  },
+  { "label": "hands.video",
+    "author": "2018",
+    "title": "bttb.project",
+    "link": "https://tom.org.nz/projects",
+    "quote": '<div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/s/8jz8a/ffbgqq?autoplay=1" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>'
+  },
+  { "label": "research.concept.mp4",
+    "author": "2017",
+    "title": "link to are.na",
+    "link": "https://are.na/tom",
+    "quote": '<div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/s/4ylec/rwsvhq?autoplay=1" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>'
+  },
+  { "label": "globe.gif",
+  "imgpath": "globe.gif",
+  "link": "https://www.buzzfeed.com/darbym4/what-cocktail-are-you-based-on-your-zodiac-sign-crsr"
+  },
+  { "label": "work.jpg",
+    "imgpath": "cat.jpg",
+    "link": "https://bttb.thac.xyz"
+  },
+  { "label": "tom.jpg",
+    "imgpath": "_portrait-2.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "workbooks.gif",
+    "imgpath": "books.gif",
+  },
+  { "label": "tom.jpg",
+    "imgpath": "_portrait-4.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "tom.jpg",
+    "imgpath": "_portrait-5.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "test-shoes.jpg",
+    "imgpath": "2.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "work.jpg",
+    "imgpath": "mars.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "sontag.jpg",
+    "imgpath": "4.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "work.jpg",
+    "imgpath": "3.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "tom.jpg",
+    "imgpath": "_portrait-11.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "tom.jpg",
+    "imgpath": "_portrait-12.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "work.jpg",
+    "imgpath": "flag.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "tom.jpg",
+    "imgpath": "_portrait-14.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "tom.jpg",
+    "imgpath": "_portrait-15.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "tom.jpg",
+    "imgpath": "_portrait-16.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "jung.jpg",
+    "imgpath": "7.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "work.jpg",
+    "imgpath": "path.jpg",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "man.gif",
+    "imgpath": "man.GIF",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "image.jpeg",
+    "imgpath": "nespresso.JPG",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "research.video",
+    "imgpath": "diary.gif",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "box.concept",
+    "imgpath": "dunhill.gif",
+    "link": "https://www.buzzfeed.com/annas451019d1e/build-a-smoothie-and-well-guess-your-birth-month-1kcr6"
+  },
+  { "label": "Snippet",
+    "author": "Aaron Shapiro",
+    "title": "Anticipatory Design",
+    "link": "https://www.fastcodesign.com/3045039/the-next-big-thing-in-design-fewer-choices",
+    "quote": 'The Internet has given us an abundance of choice and an abundance of information to inform those choices. The end result is that our lives are burdened with approximately 35,000 decisions a day.'
+  },
+  { "label": "Snippet",
+    "author": "Aaron Shapiro",
+    "title": "Anticipatory Design",
+    "link": "https://www.fastcodesign.com/3045039/the-next-big-thing-in-design-fewer-choices",
+    "quote": 'The next big breakthrough in design and technology will be the creation of products, services, and experiences that eliminate the needless choices from our lives and make ones on our behalf, freeing us up for the ones we really care about: Anticipatory design.'
+  },
+  { "label": "Concept",
+    "contents": "<b>Algorithmic Transparency</b><br> is openness about the purposes, structure and underlying actions of the algorithms used to search for, process and deliver data."
+  },
+  { "label": "Snippet",
+    "author": "Allan",
+    "title": "Pixology",
+    "link": "http://www.induceddyslexia.com/pixology.htm",
+    "quote": 'Stephen “Hawking&rsquo;s cat” is called Pixel.  He communicates with it only by computer.  The problem with Pixel is that every time as you try to look at it and get too close, the cat disappears.  It also explains why most people don&lsquo;t understand Hawking&lsquo;s cat, and why Stephen Hawking probably doesn&lsquo;t like cats.'
+  },
+  { "label": "Snippet",
+    "author": "Allan",
+    "title": "The Age of Comprehension",
+    "link": "http://www.induceddyslexia.com/default.htm",
+    "quote": 'We are no longer in The Age of Information.  We are now in The Age of Information Overload and beginning The Age of Comprehension.'
+  },
+  { "label": "Snippet",
+    "author": "Allan",
+    "title": "The Age of Comprehension",
+    "link": "http://www.induceddyslexia.com/default.htm",
+    "quote": 'While survival may have evolved from strength to knowledge, information is now so plentiful that the key to survival is no longer the acquisition of information but rather is becoming the ability to filter or weed-out extraneous information.'
+  },
+  { "label": "Snippet",
+    "author": "Allan",
+    "title": "The Age of Comprehension",
+    "link": "http://www.induceddyslexia.com/default.htm",
+    "quote": 'Knowing what to ignore is becoming almost as important as what you know, and being able to clearly see that relevant information is becoming the key to survival.'
+  },
+]
